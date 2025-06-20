514f089515d3a290bc74bacc8987f347fc145557
Thomas Koller <th.koller@outlook.com>
2025-06-20 13:48:59 +0200

    #13 #14 #4 Decoder

--- src/dnlp2025/decoder_layer.py
+++ src/dnlp2025/decoder_layer.py
@@ -2,10 +2,59 @@ import torch
 import torch.nn as nn
 
 class DecoderLayer(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, dropout=0.1) -> None:
         super(DecoderLayer, self).__init__()
-        #TODO define layers
+        #MHA1
+        self.mha1_key_projection = nn.Linear(512, 512)
+        self.mha1_query_projection = nn.Linear(512, 512)
+        self.mha1_value_projection = nn.Linear(512, 512)
+        self.mha1 = nn.MultiheadAttention(embed_dim=512, num_heads=8, dropout=dropout)
+        self.mha1_norm = nn.LayerNorm(512)
+        #MHA2
+        self.mha2_key_projection = nn.Linear(512, 512)
+        self.mha2_query_projection = nn.Linear(512, 512)
+        self.mha2_value_projection = nn.Linear(512, 512)
+        self.mha2 = nn.MultiheadAttention(embed_dim=512, num_heads=8, dropout=dropout)
+        self.mha2_norm = nn.LayerNorm(512)
+        #FFN
+        self.ffn_hidden = nn.Linear(in_features=512, out_features=2048)
+        self.relu = nn.ReLU()
+        self.ffn_output = nn.Linear(in_features=2048, out_features=512)
+        self.ffn_norm = nn.LayerNorm(512)
+        self.ffn_dropout = nn.Dropout(dropout)
 
-    def forward(self, x):
-        #TODO define forward pass
-        pass
+    def forward(self, x, encoder_out, mask=None):
+        #In "encoder-decoder attention" layers, the queries come from the previous decoder layer,
+        # and the memory keys and values come from the output of the encoder.
+        # *** MHA 1
+        residual = x
+        #output dim =512
+        key = self.mha1_key_projection(x)
+        query = self.mha1_query_projection(x)
+        value = self.mha1_value_projection(x)
+        multi_head_out = self.mha1(query, key, value, attn_mask=mask)
+        #add residual + norm
+        mha1_out = self.mha1_norm(multi_head_out + residual)
+
+        # *** MHA 2
+        residual = mha1_out
+        #output dim =512
+        # use encoder output for key and values
+        key = self.mha2_key_projection(encoder_out)
+        query = self.mha2_query_projection(x)
+        value = self.mha2_value_projection(encoder_out)
+        multi_head_out = self.mha2(query, key, value, attn_mask=mask)
+        #add residual + norm
+        mha2_out = self.mha2_norm(multi_head_out + residual)
+
+        # *** FFN
+        residual = mha2_out
+        # hidden layer
+        layer1out = self.ffn_hidden(mha2_out)
+        layer1relu = self.relu(layer1out)
+        # output layer
+        layer2out = self.ffn_output(layer1relu)
+        layer2dropout = self.ffn_dropout(layer2out)
+        # add residual + norm
+        normed = self.ffn_norm(layer2out + residual)
+        return normed
--- src/dnlp2025/encoder_layer.py
+++ src/dnlp2025/encoder_layer.py
@@ -6,11 +6,13 @@ class EncoderLayer(nn.Module):
         super(EncoderLayer, self).__init__()
         # The dimensionality of input and output is dmodel = 512, and the inner-layer has dimensionality df_f = 2048
         # FFN(x) = max(0, xW1 + b1)W2 + b2
+        #MHA
         self.key_projection = nn.Linear(512, 512)
         self.query_projection = nn.Linear(512, 512)
         self.value_projection = nn.Linear(512, 512)
         self.mha = nn.MultiheadAttention(embed_dim=512, num_heads=8, dropout=dropout)
         self.mha_norm = nn.LayerNorm(512)
+        #FFN
         self.sub2_linear_hidden = nn.Linear(in_features=512, out_features=2048)
         self.relu = nn.ReLU()
         self.sub2_linear_output = nn.Linear(in_features=2048, out_features=512)
@@ -33,10 +35,10 @@ class EncoderLayer(nn.Module):
         residual = sublayer1_out
         # hidden layer
         layer1out = self.sub2_linear_hidden(sublayer1_out)
-        layer1dropout = self.sub2_dropout(layer1out)
-        layer1relu = self.relu(layer1dropout)
+        layer1relu = self.relu(layer1out)
         # output layer
         layer2out = self.sub2_linear_output(layer1relu)
+        layer2dropout = self.sub2_dropout(layer2out)
         #add residual + norm
-        normed = self.sub2_norm(layer2out + residual)
+        normed = self.sub2_norm(layer2dropout + residual)
         return normed
