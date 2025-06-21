cf110b8dda1dbf059ce0205cb063955e70352aa3
Thomas Koller <th.koller@outlook.com>
2025-06-21 04:00:34 +0200

    reduced batch size to prevent out of memory errors

--- src/dnlp2025/train.py
+++ src/dnlp2025/train.py
@@ -59,7 +59,7 @@ def train():
     # --- Config ---
     device = get_device()
     epochs = 10
-    max_tokens_per_batch = 20000
+    max_tokens_per_batch = 2000
     learning_rate = 3e-4
     checkpoint_path = "checkpoints/aiayn.pt"
     os.makedirs("checkpoints", exist_ok=True)
